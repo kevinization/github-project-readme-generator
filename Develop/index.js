0eb37d21c9209b04f76ff14c1a4a004b1fef316b
Hilary V <92323804+hvasconez24@users.noreply.github.com>
2022-04-10 15:19:31 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,61 +1,11 @@
 // TODO: Include packages needed for this application
-<<<<<<< HEAD
-=======
 var generateMarkdown = require('./utils/generateMarkdown.js');
 var inquirer = require('inquirer');
 var fs = require('fs');
->>>>>>> 18e71dd (update repo)
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-<<<<<<< HEAD
-        type:'input',
-        name: 'title',
-        message:'What is the name of your project?',
-    },
-    {
-        type:'input',
-        name: 'Description',
-        message:'Please give a description of your project',
-    },
-    {
-        type:'input',
-        name: 'installation',
-        message:'What necessary dependencies must be imstalled to run this app?',
-    },
-    {
-        type:'input',
-        name: 'usage',
-        message:'What does the user needs to know about this repo?',
-    },
-    {
-        type:'input',
-        name: 'license',
-        message:'What kind of license should your repo use?',
-        choices: ["MIT","APACHE 2.0","GLP 3.0","BSD 3", "None"]
-    },
-    {
-        type:'input',
-        name: 'contribution',
-        message:'How can other users contribute to this app?',
-
-    },
-    {
-        type:'input',
-        name: 'test',
-        message:'How can the user test the application?',
-    },
-    {
-        type:'input',
-        name: 'username',
-        message:'Add your Github Username:',
-    },
-    {
-        type:'input',
-        name: 'email',
-        message:'Add your email address:',
-=======
         type: 'input',
         name: 'title',
         message: 'What is the name of your project?',
@@ -173,18 +123,11 @@ const questions = [
                 return false;
             }
         }
->>>>>>> 18e71dd (update repo)
     },
 
 ]
 
 // TODO: Create a function to write README file
-<<<<<<< HEAD
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-=======
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, function err(err) {
         if (err) {
@@ -202,7 +145,6 @@ function init() {
             writeToFile('README.md', generateMarkdown(userInput));
         });
 };
->>>>>>> 18e71dd (update repo)
 
 // Function call to initialize app
 init();
